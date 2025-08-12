f9b402e2367906dfcf0bb66e7ad9499a42acc3f6
ryujaen <srndpty109@gmail.com>
2025-08-12 20:59:17 +0900

    return out bgr

--- color_filter.py
+++ color_filter.py
@@ -121,7 +121,7 @@ def apply_colorblind_filter(img_bgr, color_type):
 
     # RGB -> BGR
     out_bgr = cv2.cvtColor(out_rgb, cv2.COLOR_RGB2BGR)
-    return _clip_uint8(out_rgb)
+    return _clip_uint8(out_bgr)
 
 def simulate_cvd_rgb(img_rgb: np.ndarray, cvd_type: str, severity: int = 100) -> np.ndarray:
     """
